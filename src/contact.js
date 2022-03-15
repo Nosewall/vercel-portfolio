4d2172c5d9b3abcf7061b000025a997ff3a9ab7b
Nosewall <nolannordwall@gmail.com>
2022-03-15 00:04:20 -0700

    Attempting images fix

--- src/contact.js
+++ src/contact.js
@@ -32,16 +32,16 @@ const Contact = () => {
       <p className="contactInfo">I'd love to collaborate some time soon! You can reach me using any of the links below.</p>
       <div className="contactLinks">
       <a href="https://www.linkedin.com/in/nolan-nordwall/" className="contactLink" src="">
-        <img src="/images/linkedin.png" alt="linkedIn" className="contactImg"/>
+        <img src="Images/linkedin.png" alt="linkedIn" className="contactImg"/>
       </a>
       <a href="mailto::nolannordwall@gmail.com" className="contactLink">
-        <img src="/images/email.png" alt="email" className="contactImg"/>
+        <img src="Images/email.png" alt="email" className="contactImg"/>
       </a>
       <a href="https://drive.google.com/file/d/1txy6-Ot7Tr1VypftxgarcQIjI8v5mBDh/view?usp=sharing" className="contactLink">
-        <img src="/images/attach.png" alt="attachment" className="contactImg"/>
+        <img src="Images/attach.png" alt="attachment" className="contactImg"/>
       </a>
       <a href="https://github.com/Nosewall" className="contactLink">
-        <img src="/images/github.png" alt="github" className="contactImg"/>
+        <img src="Images/github.png" alt="github" className="contactImg"/>
       </a>
       </div> 
     </motion.div>
